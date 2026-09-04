87da202075725bb2e0ae6cd90de8b8240fff9521
LeiAfkpuz <1130707+LeiAfkpuz@users.noreply.github.com>
2026-09-03 22:08:39 -0400

    v0.4.0
    Added a guard to throw an error in the future if I accidentally duplicate item IDs again.

--- plugin/Plugin.cs
+++ plugin/Plugin.cs
@@ -6,7 +6,7 @@ using HarmonyLib;
 
 namespace WitchSpringRTestPlugin
 {
-    [BepInPlugin("witchspringr.archipelago", "WitchSpring R Archipelago", "0.3.8")]
+    [BepInPlugin("witchspringr.archipelago", "WitchSpring R Archipelago", "0.4.0")]
     public class Plugin : BasePlugin
     {
         internal static ManualLogSource LogRef;
